fd9346859410dff9985832839204434dcda26e9c
emiliocorpus <emiliocorpus@gmail.com>
2016-07-18 23:03:07 -0700

    adds state to each pokemon, with remove button on mouse enter and mouse leave

--- app/assets/javascripts/components/pokemon/pokemon.jsx
+++ app/assets/javascripts/components/pokemon/pokemon.jsx
@@ -1,22 +1,51 @@
 var Pokemon = React.createClass({
+	getInitialState:function(){
+		return {
+			showDelete:false
+		}
+	},
+	showRemove:function(e){
+		e.preventDefault()
+		this.replaceState({
+			showDelete:true
+		})
+	},
+	hideRemove:function(e){
+		e.preventDefault()
+		this.replaceState({
+			showDelete:false
+		})
+	},
 	handleRemove:function(e){
 		e.preventDefault()
 		this.props.handleRemove(this.props.pokemon)
 	},
 	render:function() {
+		var display
+		if (this.state.showDelete) {
+			display = <div className="remove-poke"><a href="#" onClick={this.handleRemove}>X</a></div>
+		}
 		return (
-			<div className="pokemon-container">
-				<h2>{this.props.pokemon.id}</h2>
-				<h3>{this.props.pokemon.name}</h3>
-				<div className="pokeimage-container">
+			<div className="pokemon-container" onMouseOver={this.showRemove} onMouseLeave={this.hideRemove}>
+				
+				<div className="remove-poke-container">
+					{display}
+				</div>
+
+				<div className="poke-id">
+					<h2>{this.props.pokemon.id}</h2>
+				</div>
+
+				<div className="poke-name">
+					<h1>{this.props.pokemon.name}</h1>	
+				</div>
+				<div className="poke-image">
 					<img src={this.props.pokemon.picture_url} />
 				</div>
 				<p className="poke-description">
-					{this.props.pokemon.description}
+					Description: {this.props.pokemon.description}
 				</p>
-				<div>
-					<a href="#" onClick={this.handleRemove}>X</a>
-				</div>
+				
 			</div>
 		)
 	}
